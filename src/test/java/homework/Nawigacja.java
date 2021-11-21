ea5a0a40b7613ec890baf41c7141a406fc01b31b
nRok89 <bartosz.wozniak89@onet.pl>
2021-11-21 15:39:16 +0100

    Done

---
+++ src/test/java/homework/Nawigacja.java
@@ -0,0 +1,21 @@
+package homework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Nawigacja {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://mystore-testlab.coderslab.pl/index.php");
+
+        driver.findElement(By.className("user-info")).click();
+        driver.findElement(By.className("no-account")).click();
+        driver.navigate().back();
+        driver.navigate().back();
+
+        driver.close();
+    }
+}
