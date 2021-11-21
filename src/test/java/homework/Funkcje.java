8ccff7bb6f24325684c38330872056e7f67f20d9
nRok89 <bartosz.wozniak89@onet.pl>
2021-11-21 15:39:10 +0100

    wyrzucony zbędny kod

--- src/test/java/homework/Funkcje.java
+++ src/test/java/homework/Funkcje.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 
 public class Funkcje {
 
