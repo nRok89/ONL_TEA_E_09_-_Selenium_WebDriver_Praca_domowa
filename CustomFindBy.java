e1bb04cd42928113b8f858016aa92452eef22502
Bartosz Woźniak <90983134+nRok89@users.noreply.github.com>
2024-02-05 21:07:59 +0100

    Add files via upload

--- CustomFindBy.java
+++ CustomFindBy.java
@@ -1,24 +1,37 @@
-package baseCommends;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
-public class CustomFindBy {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-    public static WebElement initElements(CustomFindByFactory factory, Class<?> pageClassToProxy) {
-        return (WebElement) Proxy.newProxyInstance(
-                pageClassToProxy.getClassLoader(), new Class[]{WebElement.class},
-                new CustomHandler(factory)
-        );
+public class CustomFindByExample {
+
+    // Statyczna instancja WebDrivera
+    private static WebDriver driver;
+
+    public static void main(String[] args) {
+        // Inicjalizacja WebDrivera (pamiêtaj o dostosowaniu œcie¿ki do sterownika przegl¹darki)
+        System.setProperty("webdriver.chrome.driver", "œcie¿ka/do/sterownika/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        // Tworzenie obiektu klasy YourPageObject
+        YourPageObject pageObject = new YourPageObject();
+
+        // Wywo³anie metody na elemencie
+        pageObject.performSomeAction();
+
+        // Zamkniêcie przegl¹darki
+        driver.quit();
     }
 
     public static class CustomFindByFactory {
         public By buildByWithCustomFindBy(FindBy findBy) {
-            return buildBy(findBy);
+            return buildByFromFindBy(findBy);
         }
     }
 
@@ -35,14 +48,15 @@ public class CustomFindBy {
             if (findBy != null) {
                 By by = factory.buildByWithCustomFindBy(findBy);
                 // Dodaj tutaj kod obs³ugi WebDrivera i wyszukiwania elementu
-                // np. driver.findElement(by).click();
+                WebElement element = driver.findElement(by);
+                // Wykonaj odpowiednie operacje na elemencie, np. element.click();
             }
             return null;
         }
     }
 
-    @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-    @Target({java.lang.annotation.ElementType.FIELD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.FIELD)
     public @interface CustomFindBy {
         String testId() default "";
 
@@ -61,17 +75,25 @@ public class CustomFindBy {
         }
     }
 
-    // Przyk³ad u¿ycia
-    public class YourPageObject {
+    public static class YourPageObject {
 
         @CustomFindBy(testId = "username")
         private WebElement usernameField;
 
+        private CustomFindByFactory factory;
+
         public YourPageObject() {
-            CustomFindByFactory factory = new CustomFindByImpl();
-            WebElement element = CustomFindBy.initElements(factory, YourPageObject.class);
-            // Tutaj mo¿esz korzystaæ z elementu, np. element.click();
+            this.factory = new CustomFindByImpl();
+            CustomFindBy.initElements(factory, this);
+        }
+
+        public WebElement getUsernameField() {
+            return usernameField;
+        }
+
+        public void performSomeAction() {
+            // Przyk³adowa akcja na elemencie
+            getUsernameField().click();
         }
     }
 }
-
